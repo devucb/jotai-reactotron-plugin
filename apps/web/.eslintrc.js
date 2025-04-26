af6eb0f784b2796ead157ee17d88f2832f9b87c8
Serif <serifcolakel0@gmail.com>
2025-04-26 23:56:36 +0300

    refactor: rename package and remove web app files

--- apps/web/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ["@devucb/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-};
